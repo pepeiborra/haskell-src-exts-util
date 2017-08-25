8be98bc056c0eb72d6d9583cd7512f30a8cbc2f2
Pepe Iborra <pepeiborra@gmail.com>
2017-08-25 21:43:39 +0200

    Add Default orphan instances for HSE.SrcLoc types

--- src/Language/Haskell/Exts/Util.hs
+++ src/Language/Haskell/Exts/Util.hs
@@ -1,3 +1,4 @@
+{-# OPTIONS -Wno-orphans #-}
 module Language.Haskell.Exts.Util
   ( -- * Free variables of ASTs
     FreeVars(..)
@@ -12,5 +13,17 @@ module Language.Haskell.Exts.Util
 
   ) where
 
+import Data.Default
+import Language.Haskell.Exts hiding (paren)
 import Language.Haskell.Exts.Bracket
 import Language.Haskell.Exts.FreeVars
+
+-- Orphan instances required for using the bracketing code with HSE source locs
+instance Default SrcLoc where
+  def = noLoc
+  
+instance Default SrcSpan where
+  def = srcInfoSpan noSrcSpan
+
+instance Default SrcSpanInfo where
+  def = noSrcSpan
